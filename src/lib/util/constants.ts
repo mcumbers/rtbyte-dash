baa1fd3ce70c07306460014706404da82c7044dc
Michael Cumbers <me@michaelcumbers.ca>
2023-09-03 14:21:02 -0700

    Anonymize constants

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -1,10 +1,9 @@
-export const BotID: string = '1141471276268011740';
+export const BotID: string = '';
 
+// TODO: Calculate actual needed Bot Permissions
+// This currently requests Administrator perms at all times
 export const BotPermissions: string = '8';
 
-export const BotOwners: string[] = [
-	'109004714934300672',
-	'106061111605878784'
-];
+export const BotOwners: string[] = [];
 
-export const BotAPIHost: string = 'http://localhost:4000';
+export const BotAPIHost: string = '';
