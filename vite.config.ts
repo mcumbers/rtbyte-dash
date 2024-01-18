279b7f7620b791676dcc6cd5650489c17ee360cb
Michael Cumbers <me@michaelcumbers.ca>
2024-01-18 10:02:34 -0800

    Fix for Prisma Browser Types

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
 			'@configured-variables': fileURLToPath(new URL('./src/styles/variables/_template.scss', import.meta.url)),
 			'@axios': fileURLToPath(new URL('./src/plugins/axios', import.meta.url)),
 			'apexcharts': fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
+			".prisma/client/index-browser": "./node_modules/@prisma/client/index-browser.js"
 		},
 	},
 	build: {
