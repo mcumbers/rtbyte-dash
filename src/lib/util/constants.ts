d6090aaacdb5a6f11570b0f0798792ec17be558f
Michael Cumbers <me@michaelcumbers.ca>
2024-01-17 20:34:58 -0800

    Add my constants

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -9,8 +9,9 @@ export interface BotInfo {
 // This currently requests Administrator perms at all times
 export const BotPermissions: string = '8';
 
-export const DefaultBotID: string = '';
+export const DefaultBotID: string = '1141471276268011740';
 
 export const BotConnections: Map<string, BotInfo> = new Map([
-	['', { id: '', name: '', apiHost: '', redirect: '' }]
+	['1141471276268011740', { id: '1141471276268011740', name: 'stickbot indev', apiHost: 'http://localhost:4000', redirect: 'http://localhost:5173/oauth/register?id=1141471276268011740' }],
+	['1167655485529403553', { id: '1167655485529403553', name: 'ChairBot', apiHost: 'http://localhost:4000', redirect: 'http://localhost:5173/oauth/register?id=1167655485529403553' }]
 ]);
