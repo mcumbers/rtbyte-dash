102a761bcc6f409b3dd1e9ab3101f890d4bdf195
Michael Cumbers <me@michaelcumbers.ca>
2024-01-18 17:58:15 -0800

    https

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -13,6 +13,6 @@ export const DefaultBotID: string = '284970288700194818';
 
 export const BotConnections: Map<string, BotInfo> = new Map([
 	['1141471276268011740', { id: '1141471276268011740', name: 'stickbot indev', apiHost: 'http://localhost:4000', redirect: 'http://localhost:5173/oauth/register?id=1141471276268011740' }],
-	['284970288700194818', { id: '284970288700194818', name: 'stickbot', apiHost: 'https://stickbot.stickman.codes', redirect: 'http://stickbot.stickman.codes/oauth/register?id=284970288700194818' }],
+	['284970288700194818', { id: '284970288700194818', name: 'stickbot', apiHost: 'https://stickbot.stickman.codes', redirect: 'https://stickbot.stickman.codes/oauth/register?id=284970288700194818' }],
 	['1167655485529403553', { id: '1167655485529403553', name: 'ChairBot', apiHost: 'https://api.joov.sitson.me', redirect: 'https://joov.sitson.me/oauth/register?id=1167655485529403553' }]
 ]);
